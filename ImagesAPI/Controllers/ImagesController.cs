3c65eb2d1ecb2c6e252893c0b2321efe7fd5593b
Radek Jakubowski <jakubowskiumieszcz@gmail.com>
2023-08-19 13:08:19 +0200

    working poc

---
+++ ImagesAPI/Controllers/ImagesController.cs
@@ -0,0 +1,24 @@
+﻿using ImagesAPI.Services.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImagesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImagesService _imagesService;
+
+        public ImagesController(IImagesService imagesService)
+        {
+            _imagesService = imagesService;
+        }
+
+        [HttpPost]
+        public async Task UploadImage(IFormFile imageFile)
+        {
+            await _imagesService.UploadImage(imageFile);
+        } 
+    }
+}
--- ImagesAPI/Services/CloudinaryImagesService.cs
+++ ImagesAPI/Services/CloudinaryImagesService.cs
@@ -5,6 +5,7 @@ using ImagesAPI.Helpers;
 using ImagesAPI.Helpers.Abstractions;
 using ImagesAPI.Services.Abstractions;
 using Microsoft.Extensions.Options;
+using System.Diagnostics;
 
 namespace ImagesAPI.Services
 {
@@ -13,8 +14,9 @@ namespace ImagesAPI.Services
         private readonly Cloudinary _cloudinaryClient;
         private readonly ICloudinaryAccountFactory _cloudinaryAccountFactory;
 
-        public CloudinaryImagesService(IOptions<CloudinaryConfig> cloudinaryConfiguration, ICloudinaryAccountFactory cloudinaryAccountFactory)
+        public CloudinaryImagesService(ICloudinaryAccountFactory cloudinaryAccountFactory)
         {
+            _cloudinaryAccountFactory = cloudinaryAccountFactory;
             _cloudinaryClient = new Cloudinary(_cloudinaryAccountFactory.CreateCloudinaryAccount());
         }
 
@@ -28,6 +30,8 @@ namespace ImagesAPI.Services
 
             var imageUploadResponse = await _cloudinaryClient.UploadAsync(imageUploadParams);
 
+            Process.Start(new ProcessStartInfo(imageUploadResponse.SecureUrl.ToString()) { UseShellExecute = true });
+            // for now I just open the browser tab with link to uploaded file
             // rest of the stuff like sending some domain event or persisting it to db
         }
     }
